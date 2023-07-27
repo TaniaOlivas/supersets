2c2254320a6237cd13a865855ab333a931fcce30
TaniaOlivas <taniaolivas24@gmail.com>
2023-07-26 22:32:21 -0400

    Comments added and cleaned up

--- main.py
+++ main.py
@@ -1,29 +1,43 @@
+"""
+This main.py module sets up the main window of the program.
+It includes two buttons, one that will open up another window
+and another that will exit the program.
+"""
+
+# Imports tkinter and PIL to use for the GUI
+# Imports the loggingWindow function from the logging.py module
 import tkinter as tk
 from src.logging import loggingWindow
 from PIL import ImageTk, Image
 
-
-root = tk.Tk()
+# Initializes the main window 
+root = tk.Tk() # Variable assigned to initialized window to call to it easier
 root.title("Home")
 root.geometry('400x400')
 
+# Opens and resizes the dumbbell picture
 db = Image.open("/Users/taniaolivas/Documents/SDEV140/superSets/src/dumbbells.png")
 resize_db = db.resize((100,100))
 dumbbells = ImageTk.PhotoImage(resize_db)
 
+# Opens and resizes the stopwatch picture
 sw = Image.open("/Users/taniaolivas/Documents/SDEV140/superSets/src/stopwatch.png")
 resize_sw = sw.resize((100,100))
 stopwatch = ImageTk.PhotoImage(resize_sw)
 
+# First label to ask the user what they would like to do
 tk.Label(root,text="What would you like to do?").pack()
 
+# Provides a button that will take them to the logging page once clicked
 loggingButton = tk.Button(root, text="Log your workout", command=loggingWindow)
 loggingButton.pack()
 
+# An exit button to leave the window
 exitButton = tk.Button(root, text="Exit", command=root.destroy)
 exitButton.pack()
 
-dumbbellImage = tk.Label(root, image=dumbbells).pack(side=tk.LEFT)
+# Two labels that have images instead of text
+dumbbellImage = tk.Label(root, image=dumbbells).pack(side=tk.LEFT) 
 stopwatchImage = tk.Label(root, image=stopwatch).pack(side=tk.RIGHT)
 
 
--- src/logging.py
+++ src/logging.py
@@ -1,45 +1,81 @@
+"""
+The logging.py module runs the second window opened in
+the application. Here a user is introduced with entries
+to log their workout information. There is also data 
+validation functions within this page to validate the
+user entries. 
+"""
+
+# Imports the necessary tools for the GUI
 import tkinter as tk
 from tkinter import messagebox
 
+# The main function that is fired once the button from the 
+# main page is clicked
 def loggingWindow():
-    new = tk.Toplevel()
+    
+    new = tk.Toplevel() # Initialized a new window to open
     new.geometry("650x250")
     new.title("Log Your Workout")
-    nameValidation = new.register(validate_name)
-    lengthValidation = new.register(validate_length)
-    caloriesValidation = new.register(validate_calories)
-    rateValidation = new.register(validate_rate)
+
+    # Initializes the valiation functions to assign them to each of 
+    # the entry boxes (name, length, calories, rate)
+    nameValidation = new.register(validate_name) # Initializes the validate_name function
+    lengthValidation = new.register(validate_length) # Initializes the validate_length function
+    caloriesValidation = new.register(validate_calories) # Initializes the validate_calories function
+    rateValidation = new.register(validate_rate) # Initializes the validate_rate function
+
+    # Creates a label and entry to the Workout Name entry
     tk.Label(new, text="Workout name:").pack()
-    workoutName = tk.Entry(new, textvariable="name")
+    workoutName = tk.Entry(new, textvariable="name") # Assigns the entry to a variable to call later
+
+    # Allows for data validation on the workout name entry
     workoutName.config(validate="focusout", validatecommand=(nameValidation,"%P"))
     workoutName.pack()
+
+    # Creates a label and entry to the Workout Length entry
     tk.Label(new, text="Workout length:").pack()
-    length = tk.Entry(new, textvariable="length")
+    length = tk.Entry(new, textvariable="length") # Assigns the entry to a variable to call later
+    # Allows for data validation on the workout length entry
     length.config(validate="focusout", validatecommand=(lengthValidation,"%P"))
     length.pack()
+
+    # Creates a label and entry to the calories entry
     tk.Label(new, text="Amount of calories burned:").pack()
-    calories = tk.Entry(new, textvariable="calories")
+    calories = tk.Entry(new, textvariable="calories") # Assigns the entry to a variable to call later
+    # Allows for data validation on the calories entry
     calories.config(validate="focusout", validatecommand=(caloriesValidation,"%P"))
     calories.pack()
+
+    # Creates a label and entry to the rate entry
     tk.Label(new, text="Rate the workout from 1-10:").pack()
-    rate = tk.Entry(new, textvariable="rate")
+    rate = tk.Entry(new, textvariable="rate") # Assigns the entry to a variable to call later
+    # Allows for data validation on the rate entry
     rate.config(validate="focusout", validatecommand=(rateValidation,"%P"))
     rate.pack()
-    tk.Button(new, text="Submit", command=lambda: addToLog(workoutName,length,calories,rate)).pack()
+
+    # Creates a submit button once all the information is filled out
+    submitButton = tk.Button(new, text="Submit", command=addToLog) # Assigns the entry to a variable to call later
+    submitButton.pack()
+
+    # Creates an exit button to escape from this new window and return to the root window
+    exitButton = tk.Button(new, text="Exit", command=new.destroy) # Assigns the entry to a variable to call later
+    exitButton.pack()
 
 
-def addToLog(workoutName, length, calories, rate):
-    print(workoutName.get())
-    print(length.get())
-    print(calories.get())
-    print(rate.get())
+# Function that fires once the submit button is clicked
+def addToLog():
+    # A message box appears to confirm that the workout was submitted
+    messagebox.showinfo('Submitted', 'Your workout was submitted!')
 
+# Validation function that checks for an empty field
 def validate_name(input):
     if input == "":
         messagebox.showerror('Error', 'This field cannot be empty, please provide a name.')
     else:
         return True
 
+# Validation function that checks for an empty field and if the characters are numbers
 def validate_calories(input):
     if input == "":
         messagebox.showerror('Error', 'This field cannot be empty, please provide the amount of calories burned.')
@@ -50,6 +86,7 @@ def validate_calories(input):
             messagebox.showerror('Error', 'Please enter a valid number between 0-10000.')
             return False
 
+# Validation function that checks for an empty field and if the characters are numbers
 def validate_length(input):
     if input == "":
         messagebox.showerror('Error', 'This field cannot be empty, please provide the length of the workout.')
@@ -59,7 +96,8 @@ def validate_length(input):
         else:
             messagebox.showerror('Error', 'Please enter a valid number of minutes between 0-10000.')
             return False
-        
+
+# Validation function that checks for an empty field and if the characters are numbers       
 def validate_rate(input):
     if input == "":
         messagebox.showerror('Error', 'This field cannot be empty, please rate the workout.')
