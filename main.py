0e99d1da0d7480e312a4487c2f51b4d1921f1302
TaniaOlivas <taniaolivas24@gmail.com>
2023-07-26 22:07:04 -0400

    Added images to main page

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
 import tkinter as tk
 from src.logging import loggingWindow
-from src.journal import journalWindow
-
+from PIL import ImageTk, Image
 
 
 root = tk.Tk()
 root.title("Home")
-root.geometry("750x250")
+root.geometry('400x400')
+
+db = Image.open("/Users/taniaolivas/Documents/SDEV140/superSets/src/dumbbells.png")
+resize_db = db.resize((100,100))
+dumbbells = ImageTk.PhotoImage(resize_db)
+
+sw = Image.open("/Users/taniaolivas/Documents/SDEV140/superSets/src/stopwatch.png")
+resize_sw = sw.resize((100,100))
+stopwatch = ImageTk.PhotoImage(resize_sw)
 
 tk.Label(root,text="What would you like to do?").pack()
-
 
 loggingButton = tk.Button(root, text="Log your workout", command=loggingWindow)
 loggingButton.pack()
 
-journalButton = tk.Button(root, text="View your journal", command=journalWindow)
-journalButton.pack()
-
 exitButton = tk.Button(root, text="Exit", command=root.destroy)
 exitButton.pack()
+
+dumbbellImage = tk.Label(root, image=dumbbells).pack(side=tk.LEFT)
+stopwatchImage = tk.Label(root, image=stopwatch).pack(side=tk.RIGHT)
+
 
 root.mainloop()
--- src/journal.py
+++
@@ -1,7 +0,0 @@
-import tkinter as tk
-
-def journalWindow():
-    new = tk.Toplevel()
-    new.geometry("650x250")
-    new.title("Your Workout Journal")
-    tk.Label(new, text="Journal Window").pack()
